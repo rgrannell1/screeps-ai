8989e26d38b667930b35c0636c1e0e1319217c74
rgrannell1 <r.grannell2@gmail.com>
2018-09-16 22:30:03 +0100

    now indexes documents

--- src/show-events/index.js
+++ src/show-events/index.js
@@ -2,13 +2,26 @@
 const loki = require('lokijs')
 const path = require('path')
 
+const chai = require('chai')
+const request = require('request-promise-native')
+const chaiJsonSchema = require('chai-json-schema')
+chai.use(chaiJsonSchema)
+
 async function main () {
   const lk = new loki(path.join(__dirname, '../../data/screeps-events.json'))
-  const lkEvents = lk.addCollection('events')
+  lk.loadDatabase({}, async () => {
+    const lkEvents = lk.getCollection('events')
 
+    const results = lkEvents.find()
 
+    for (const doc of results) {
 
-  lk.saveDatabase()
+      await request.post(`http://localhost:9200/events/_doc/${doc.id}`, {
+        json: doc
+      })
+
+    }
+  })
 }
 
 main()
