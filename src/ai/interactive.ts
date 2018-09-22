2b52e1d33bcd5f8b7ef692aa60052d4ee16e0a81
rgrannell1 <r.grannell2@gmail.com>
2018-09-22 21:56:54 +0100

    added more interactive commands

--- src/ai/interactive.ts
+++ src/ai/interactive.ts
@@ -1,4 +1,14 @@
 
 const interactive = {} as any
 
+interactive.clearPlans = () => {
+  Memory.plans = []
+  Object.values(Game.constructionSites).forEach(site => site.remove())
+}
+
+interactive.clearFlags = () => {
+  Object.values(Game.flags).forEach(flag => flag.remove())
+}
+
 export default interactive
+global.interactive = interactive
