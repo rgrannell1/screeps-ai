9eac8c686aed870295b8791772ce6bbc7b7d4f4a
rgrannell1 <r.grannell2@gmail.com>
2018-09-24 21:47:45 +0100

    patched event upload

--- src/show-events/index.js
+++ src/show-events/index.js
@@ -50,19 +50,45 @@ elasticsearch.setEventMapping = async function () {
   return request.put(`http://localhost:9200/_template/events`, {json: mapping})
 }
 
+const sql = {}
+sql.retrieveEvents = `
+SELECT *
+FROM events
+ORDER BY id
+LIMIT ?, ?;
+`
+
+sql.countEvents = `
+SELECT count(*) AS event_count
+FROM events;
+`
+
 async function main (emitter, event) {
   const fpath = path.join(__dirname, '../../data/screeps-events.sqlite')
   const sqlDb = await sqlite.open(fpath)
 
-  const results = await sqlDb.all('SELECT * FROM events', [])
-
   await elasticsearch.setEventMapping()
-  emitter.emit(event, `writing ${results.length} events to ElasticSearch.`)
+  const [countResult] = await sqlDb.all(sql.countEvents, [])
 
-  for (const doc of results) {
-    await request.post(`http://localhost:9200/events/_doc/${doc.id}`, {json: JSON.parse(doc.content)})
+  emitter.emit(event, `writing ${countResult.event_count} events to ElasticSearch.`)
+
+  let skip = 0
+  const batch = 250
+
+  while (true) {
+    let results = await sqlDb.all(sql.retrieveEvents, [skip, batch])
+    if (results.length === 0) {
+      return
+    } else {
+      emitter.emit(event, `writing ${skip}...${skip + batch} of ${countResult.event_count} events to ElasticSearch`)
+    }
+
+    for (const doc of results) {
+      await request.post(`http://localhost:9200/events/_doc/${doc.id}`, {json: JSON.parse(doc.content)})
+    }
+
+    skip += 100
   }
-
 }
 
 module.exports = main
